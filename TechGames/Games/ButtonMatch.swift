d36efc33a043ab331921e0a7b498d0486274e76c
cagatay.yildiz <cagatay@pixupplay.com>
2024-09-05 20:21:44 +0300

    simon says

---
+++ TechGames/Games/ButtonMatch.swift
@@ -0,0 +1,31 @@
+//
+//  SimonSays.swift
+//  TechGames
+//
+//  Created by Çağatay Yıldız on 5.09.2024.
+//
+
+import SwiftUI
+
+struct SimonSays: View {
+    @State var color : Color = .gray
+    @State var selectedColor : Color = .gray
+    var body: some View {
+        VStack{
+       
+                ForEach(1..<16) { i in
+                    
+                    
+                    Button("Hello"){
+                        
+                    }
+                 }
+            
+           
+        }
+    }
+}
+
+#Preview {
+    SimonSays()
+}
